19906e234fb31fe6fc279cfc672dd2efa6145db1
YD <ydglitch@gmail.com>
2022-03-11 12:04:35 +0000

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,26 @@
-
+import contImg from './utils/svgs/contact.svg'
+import { Row, Col } from 'antd'
 const Contact = () =>  {
 
     return (
         <div>
+             <Row >
+            <Col xs={{span:24}} sm={{span:24}} md={{span:12}} lg={{span:0}} >
+            <div className="">
+              <img src={contImg} className="img-fluid" alt="svg" loading="lazy" />
+            </div>
+            </Col>
+              <Col sm={{span:24}} lg={{span:12}} md={{span:12}}>
+                
+                
 
+              </Col>
+              <Col xs={{span:0}} sm={{span:0}} md={{span:0}} lg={{span:12}} >
+              <div className="">
+                <img src={contImg} className="img-fluid" alt="svg" loading="lazy" />
+              </div>
+              </Col>
+            </Row>
         </div>
             
     )
