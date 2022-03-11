ab38332744c8a7e9f0166c930fc48147344c8e91
YD <ydglitch@gmail.com>
2022-03-11 12:25:58 +0000

    projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,21 +3,22 @@ import { Row, Col } from 'antd'
 const Contact = () =>  {
 
     return (
-        <div>
+        <div className="container">
+                <h2 className="mb-4"> Contact. </h2>
              <Row >
             <Col xs={{span:24}} sm={{span:24}} md={{span:12}} lg={{span:0}} >
             <div className="">
-              <img src={contImg} className="img-fluid" alt="svg" loading="lazy" />
+              <img src={contImg} className="img-fluid my-3" alt="svg" loading="lazy" />
             </div>
             </Col>
               <Col sm={{span:24}} lg={{span:12}} md={{span:12}}>
-                
+
                 
 
               </Col>
               <Col xs={{span:0}} sm={{span:0}} md={{span:0}} lg={{span:12}} >
               <div className="">
-                <img src={contImg} className="img-fluid" alt="svg" loading="lazy" />
+                <img src={contImg} className="img-fluid my-3" alt="svg" loading="lazy" />
               </div>
               </Col>
             </Row>
