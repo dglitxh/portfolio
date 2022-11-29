1df2652a004f1f5ae6749a47e1ec30c72675d1bc
ydeezly <ydglitch@gmail.com>
2022-11-29 11:15:20 +0000

    upd: contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,58 @@
-import contImg from './utils/svgs/contact.svg'
-import { Row, Col } from 'antd'
-const Contact = () =>  {
+import contImg from "./utils/svgs/contact.svg";
+import { Row, Col } from "antd";
+const Contact = () => {
+  return (
+    <div id="contact" className="container">
+      <h2 className="mb-4"> Contact. </h2>
+      <Row>
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 12 }}
+          lg={{ span: 0 }}
+        >
+          <div>
+            <img
+              src={contImg}
+              className="img-fluid my-3"
+              alt="svg"
+              loading="lazy"
+            />
+          </div>
+        </Col>
+        <Col sm={{ span: 24 }} lg={{ span: 12 }} md={{ span: 12 }}>
+          <div className="contacts">
+            <h3 classname="mt-3">You can always shoot me an email on: </h3>
+            <br></br>
+            <h3 classname="my-5">
+              <a
+                href="mailto:dzzy104@gmail.com"
+                rel="noreferrer"
+                target="_blank"
+              >
+                email{" "}
+              </a>
+            </h3>
+          </div>
+        </Col>
+        <Col
+          xs={{ span: 0 }}
+          sm={{ span: 0 }}
+          md={{ span: 0 }}
+          lg={{ span: 12 }}
+        >
+          <div className="">
+            <img
+              src={contImg}
+              className="img-fluid my-3"
+              alt="svg"
+              loading="lazy"
+            />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-    return (
-        <div id="contact" className="container">
-                <h2 className="mb-4"> Contact. </h2>
-             <Row >
-            <Col xs={{span:24}} sm={{span:24}} md={{span:12}} lg={{span:0}} >
-            <div>
-              <img src={contImg} className="img-fluid my-3" alt="svg" loading="lazy" />
-            </div>
-            </Col>
-              <Col sm={{span:24}} lg={{span:12}} md={{span:12}}>
-                <div className="contacts">
-                  <h3 classname="mt-3">You can always shoot me an email on: </h3>
-                  <br></br>
-                <h3 classname="my-5">
-                <a href="http://ydee.netlify.app" rel='noreferrer' target="_blank">email </a>
-                
-                 </h3>
-
-                </div>
-              </Col>
-              <Col xs={{span:0}} sm={{span:0}} md={{span:0}} lg={{span:12}} >
-              <div className="">
-                <img src={contImg} className="img-fluid my-3" alt="svg" loading="lazy" />
-              </div>
-              </Col>
-            </Row>
-        </div>
-
-    )
-}
-
-
-export default Contact
+export default Contact;
