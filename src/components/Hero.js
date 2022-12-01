60c159350691701027fb7f53d21e3a6eb7afdb33
ydeezly <ydglitch@gmail.com>
2022-12-01 21:09:22 +0000

    lint

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,81 +1,94 @@
+import React from "react";
+import { Row, Col } from "antd";
+import { motion } from "framer-motion";
+import coderSvg from "./utils/svgs/codred.svg";
 
-        import React from 'react';
-        import { Row, Col } from 'antd'
-        import { motion } from 'framer-motion';
-        import coderSvg from './utils/svgs/codred.svg'
+const Hero = () => {
+  //   const containerVariants = {
+  //     hidden: {
+  //     y: '100vh',
+  //   },
+  //   visible: {
+  //     y: 0,
+  //     transition: { delay: 0.2, duration: 0.5 }
+  //   },
+  //   exit: {
+  //     x: "-100vh",
+  //     transition: { ease: 'easeInOut' }
+  //   }
+  // };
 
+  return (
+    <motion.div
+      id="home"
+      className="container mt-5 mb-5 d-flex flex-column h-100"
+      // variants={containerVariants}
+      // initial='hidden'
+      // animate="visible"
+      // exit="exit"
+    >
+      <Row>
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 12 }}
+          lg={{ span: 0 }}
+        >
+          <div className="hero-img">
+            <motion.img
+              animate={{ scale: [0.2, 1.5, 1] }}
+              transition={{ ease: "easeOut", duration: 2 }}
+              src={coderSvg}
+              className="img-fluid"
+              alt="svg"
+              loading="lazy"
+            />
+          </div>
+        </Col>
+        <Col sm={{ span: 24 }} lg={{ span: 12 }} md={{ span: 12 }}>
+          <br />
+          <motion.div
+            className="hero"
+            animate={{ x: [-1000, 100, -100, 0] }}
+            transition={{ ease: "easeOut", duration: 2 }}
+          >
+            <h1 className="big-hero">
+              Yaw<br></br>
+              <span>Deezy.</span>
+            </h1>
 
+            <p className=" mb-4">
+              I develop fullstack Applications with Go, Python and JavaScript.
+            </p>
 
+            <a
+              className="mr-3 hero-button btn btn-primary btn-large"
+              href="#projects"
+            >
+              Projects
+            </a>
+          </motion.div>
+        </Col>
+        <Col
+          xs={{ span: 0 }}
+          sm={{ span: 0 }}
+          md={{ span: 0 }}
+          lg={{ span: 12 }}
+        >
+          <div className="hero-img">
+            <motion.img
+              animate={{ scale: [0.2, 1.5, 1] }}
+              transition={{ ease: "easeOut", duration: 2 }}
+              src={coderSvg}
+              className="img-fluid"
+              alt="svg"
+              loading="lazy"
+            />
+          </div>
+        </Col>
+      </Row>
+    </motion.div>
+  );
+};
 
-        const Hero = () =>  {
-
-
-        //   const containerVariants = {
-        //     hidden: {
-        //     y: '100vh',
-        //   },
-        //   visible: {
-        //     y: 0,
-        //     transition: { delay: 0.2, duration: 0.5 }
-        //   },
-        //   exit: {
-        //     x: "-100vh",
-        //     transition: { ease: 'easeInOut' }
-        //   }
-        // };
-
-            return (
-                <motion.div id="home" className='container mt-5 mb-5 d-flex flex-column h-100'
-              // variants={containerVariants}
-              // initial='hidden'
-              // animate="visible"
-              // exit="exit"
-              >
-            <Row >
-            <Col xs={{span:24}} sm={{span:24}} md={{span:12}} lg={{span:0}} >
-            <div className="hero-img">
-              <motion.img
-                animate={{ scale: [0.2, 1.5, 1] }}
-                transition={{ ease: "easeOut", duration: 2 }}
-               src={coderSvg} className="img-fluid" alt="svg" loading="lazy" />
-            </div>
-            </Col>
-              <Col sm={{span:24}} lg={{span:12}} md={{span:12}}>
-
-                      <br/>
-              <motion.div className="hero"
-                   animate={{ x: [-1000, 100, -100, 0]}}
-                   transition={{ ease: "easeOut", duration: 2 }}
-               >
-
-                <h1 className="big-hero">
-                  Yaw<br></br><span>Deezy.</span>
-                </h1>
-
-                <p className=" mb-4">I develop fullstack Applications with Go, Python and JavaScript.
-
-                </p>
-
-                  <a className="mr-3 hero-button btn btn-primary btn-large" href='#projects'>
-                     Projects
-                  </a>
-
-
-
-              </motion.div>
-              </Col>
-              <Col xs={{span:0}} sm={{span:0}} md={{span:0}} lg={{span:12}} >
-              <div className="hero-img">
-                <motion.img
-                  animate={{ scale: [0.2, 1.5, 1] }}
-                  transition={{ ease: "easeOut", duration: 2 }}
-                 src={coderSvg} className="img-fluid" alt="svg" loading="lazy" />
-              </div>
-              </Col>
-            </Row>
-                </motion.div>
-            )
-        }
-
-
-export default Hero
+export default Hero;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,54 +1,90 @@
-import {Row, Col, Image, Button } from 'antd'
-import {projects} from './assets/myprojects'
+import { Row, Col, Image, Button } from "antd";
+import { projects } from "./assets/myprojects";
 
-
-const Projects = ( ) => {
-
-    return (
-        <div id="projects" className="container">
-
-        <h2 className=' mt-5 mb-3'> Projects </h2>
-<Row>
-{projects.map((project) => {
-return(<Col key={project.id}  xs={{span:24}} sm={{span:24}} md={{span:12}} lg={{span:8}}>
-        <div className="card rounded-3 mb-5 m-3 project_card">
-          <div className="px-2 ">
-            <div>
-           <Image src={project.img} alt="pic" className="mt-n6 pt-2 img-fluid w-100 smooth-shadow-md rounded-3"/>
-            </div>
-          </div>
-          <div className="card-body px-4 py-5 p-md-8">
-            <div className="border-bottom pb-3 mb-5">
-              <h3 className="proj-name mb-2 display-7"> {project.name}</h3>
-              <p className="lead">{project.desc}</p>
-              <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap'}}>
-                {project.stack.map(el=>{
-                  return(
-                    <Button ghost key={el+String(Math.random(20))} className={"m-1 stack-ls"} shape='round'>{el}</Button>
-                  )
-                })}
+const Projects = () => {
+  return (
+    <div id="projects" className="container">
+      <h2 className=" mt-5 mb-3"> Projects </h2>
+      <Row>
+        {projects.map((project) => {
+          return (
+            <Col
+              key={project.id}
+              xs={{ span: 24 }}
+              sm={{ span: 24 }}
+              md={{ span: 12 }}
+              lg={{ span: 8 }}
+            >
+              <div className="card rounded-3 mb-5 m-3 project_card">
+                <div className="px-2 ">
+                  <div>
+                    <Image
+                      src={project.img}
+                      alt="pic"
+                      className="mt-n6 pt-2 img-fluid w-100 smooth-shadow-md rounded-3"
+                    />
+                  </div>
+                </div>
+                <div className="card-body px-4 py-5 p-md-8">
+                  <div className="border-bottom pb-3 mb-5">
+                    <h3 className="proj-name mb-2 display-7">
+                      {" "}
+                      {project.name}
+                    </h3>
+                    <p className="lead">{project.desc}</p>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flexWrap: "wrap",
+                      }}
+                    >
+                      {project.stack.map((el) => {
+                        return (
+                          <Button
+                            ghost
+                            key={el + String(Math.random(20))}
+                            className={"m-1 stack-ls"}
+                            shape="round"
+                          >
+                            {el}
+                          </Button>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="d-flex flex-row justify-content-center">
+                    {!project.site ? (
+                      <></>
+                    ) : (
+                      <a
+                        href={project.site}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="btn btn-outline-primary mx-2"
+                      >
+                        Live demo <i className="fe fe-arrow-right"></i>
+                      </a>
+                    )}
+                    <a
+                      href={project.github}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="btn btn-outline-primary mx-2"
+                    >
+                      github <i className="fe fe-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="d-flex flex-row justify-content-center">
-              {!project.site? <></> : <a href={project.site} target="_blank" rel="noreferrer" className="btn btn-outline-primary mx-2">
-                Live demo <i className="fe fe-arrow-right"></i>
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
+};
 
-              </a>}
-              <a href={project.github} target="_blank" rel="noreferrer" className="btn btn-outline-primary mx-2">
-                github <i className="fe fe-arrow-right"></i>
-
-              </a>
-            </div>
-
-          </div>
-        </div>
-        </Col>    )
-      })
-}
-        </Row>
-
-        </div>
-    )
-}
-
-export default Projects
+export default Projects;
