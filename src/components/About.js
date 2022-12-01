086cee90959338082636831f42b4ff2859ba457c
ydeezly <ydglitch@gmail.com>
2022-12-01 21:06:39 +0000

    upd: about

--- src/components/About.js
+++ src/components/About.js
@@ -1,57 +1,59 @@
-import { Row, Col } from 'antd';
-import { skills } from './assets/data'
-import { motion } from 'framer-motion'
+import { Row, Col } from "antd";
+import { skills } from "./assets/data";
+import { motion } from "framer-motion";
 
-
-const Services = () => {
-
+const About = () => {
   const containerVariants = {
     hidden: {
       opacity: 0,
-      x: '-100vh'
+      x: "-100vh",
     },
     visible: {
       opacity: 1,
       x: 0,
-      transition: { type: 'spring', duration: 4 }
+      transition: { type: "spring", duration: 4 },
     },
     exit: {
       x: "-100vw",
-      transition: { ease: 'easeInOut' }
-    }
+      transition: { ease: "easeInOut" },
+    },
+  };
 
+  return (
+    <motion.div
+      id="about"
+      className="services container my-5 mb-10"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h2 className="mb-4"> What I Do</h2>
+      <Row className="mt-5">
+        {skills.map((service) => {
+          return (
+            <Col
+              key={service.id}
+              xs={{ span: 24 }}
+              sm={{ span: 12 }}
+              lg={{ span: 8 }}
+              md={{ span: 8 }}
+            >
+              <div
+                data-aos="zoom-out-down"
+                data-aos-duration="1500"
+                className="info-card my-4"
+              >
+                <img alt="icon" src={service.icon} />
+                <p className="my-2 display-5 card-h4"> {service.heading} </p>
+                <p class="card-text">{service.text}</p>
+              </div>
+            </Col>
+          );
+        })}
+      </Row>
+    </motion.div>
+  );
 };
 
-    return (
-        <motion.div id="about" className="services container my-5 mb-10"
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
-        exit="exit"
-      >
-        <h2 className="mb-4"> What I Do</h2>
-      <Row className="mt-5">
-      {skills.map((service) => {
-        return(
-          <Col key={service.id} xs={{span:24}} sm={{span:12}} lg={{span:8}} md={{span:8}}>
-            <div  data-aos="zoom-out-down" data-aos-duration="1500" className="info-card my-4">
-              <img alt="icon" src={service.icon}/>
-            <p className='my-2 display-5 card-h4'> {service.heading} </p>
-              <p class="card-text">{service.text}</p>
-      </div>
-
-      </Col>
-        )
-      })
-     }
-
-      </Row>
-
-
-
-</motion.div>
-    )
-}
-
-
-export default Services
+export default About;
